f06b73762eeac1045bb9e3bf50f9755840dc8b79
Dvir Akerman <excaliburfrc@gmail.com>
2020-05-18 14:41:13 +0300

    Added simple unit tests

---
+++ test_all.py
@@ -0,0 +1,52 @@
+from Lexer import Lexer
+from Parser import Parser
+from Runner import Runner
+
+def run_code(code: str):
+    lexer = Lexer()
+    tokens = lexer.tokenize(code)
+
+    parser = Parser(tokens)
+    node_tree = parser.parse()
+
+    runner = Runner(node_tree)
+    return runner.run()
+
+def test_math():
+    code = """
+    a = 5;
+    b = 10;
+    return (a+b);
+    """
+
+    assert run_code(code) == 15
+
+def test_if():
+    code = """
+    a = 5;
+    b = 10;
+    statement = (a < b) + !(b < a) + (a != b);
+    if (statement == 3){
+        return 37;
+    }
+    return 10;
+    """
+
+    assert run_code(code) == 37
+
+def test_nested_ifs():
+    code = """
+    a = 5;
+    b = 10;
+    if (a < b){
+        if (a != b){
+            return 37;
+        }
+        return 10;
+    }
+    return 10;
+    """
+
+    assert run_code(code) == 37
+
+
